50fcf3fdbb3d653150d1d6d663c6e39605a9c8f2
Pratik Jindal <117007380+jindalpratik@users.noreply.github.com>
2024-03-06 18:41:16 +0530

    Remove aditional code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
@@ -9,7 +8,6 @@ import Results from "./components/Results";
 import LangSelect from "./components/LangSelect";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
